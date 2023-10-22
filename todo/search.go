a38bec67aca84cb1855764de9e46907e365b04dd
Rafael Oliveira <ra.pha@live.com>
2023-10-21 21:29:47 -0300

    feat(search): parse files in parallel

--- todo/search.go
+++ todo/search.go
@@ -3,7 +3,6 @@ package todo
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"io/fs"
 	"log"
@@ -12,93 +11,87 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/fulecorafa/panoptes/pipeline"
 	sitter "github.com/smacker/go-tree-sitter"
 )
 
 func getTodos(ctx context.Context) ([]Todo, error) {
-    ctx, cancel := context.WithCancelCause(ctx)
-    defer cancel(nil)
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer cancel(nil)
 
-    /* PIPELINE */
-    // Search all files
-    pathsChan := walkdirGenerator(ctx, cancel)
-    for path := range pathsChan {
-        fmt.Println(path)
-    }
-    return nil, nil
+    /* Context variables */
+	workerN := ctx.Value("throughput").(int)
+
+	/* PIPELINE */
+	// Search all files
+	pathsChan := walkdirGenerator(ctx, cancel)
+	parserChan := pipeline.RunInParallel(ctx, cancel, workerN, pathsChan, parseFile)
+	return nil, nil
 }
 
-func parseFile(ctx context.Context, files <-chan sourceFile, results chan<- *sitter.Tree) error {
-    for file := range files {
-        fileReader, err := os.Open(file.path)
-        if err != nil {
-            return err
-        }
+func parseFile(ctx context.Context, file sourceFile) (*sitter.Tree, error) {
+	fileReader, err := os.Open(file.path)
+	if err != nil {
+		return nil, err
+	}
 
-        lang, ok := decideLanguage(file.info)
-        if !ok {
-            return errors.New("could not detect file language")
-        }
+	lang, ok := decideLanguage(file.info)
+	if !ok {
+		return nil, errors.New("could not detect file language")
+	}
 
-        source, err := io.ReadAll(fileReader)
-        if err != nil {
-            return err
-        }
+	source, err := io.ReadAll(fileReader)
+	if err != nil {
+		return nil, err
+	}
 
-        parser := sitter.NewParser()
-        parser.SetLanguage(lang)
-        tree, err := parser.ParseCtx(ctx, nil, source)
-        if err != nil {
-            return err
-        }
-
-        select {
-        case results <- tree:
-        case <- ctx.Done():
-            return nil
-        }
-    }
-    return nil
+	parser := sitter.NewParser()
+	parser.SetLanguage(lang)
+	tree, err := parser.ParseCtx(ctx, nil, source)
+	if err != nil {
+		return nil, err
+	}
+    return tree, nil
 }
 
-func walkdirGenerator(ctx context.Context, cancelCauseFunc context.CancelCauseFunc) (<-chan sourceFile) {
-    log.Default().Println("Walking dir")
-    pathsChan := make(chan sourceFile)
-    root := ctx.Value("root").(string)
-    extensions := ctx.Value("fileExtensions").([]string)
-    ignoredDirs := ctx.Value("ignoredDirs").([]string)
-    go func() {
-        defer close(pathsChan)
-        err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
-            log.Default().Println("Searching ", path)
-            if err != nil {
-                return err
-            }
-            if !info.Type().IsRegular() {
-                return nil
-            }
-            // Skip directories
-            if info.IsDir() && slices.Contains(ignoredDirs, info.Name()) {
-                log.Default().Println("Skipping ", path)
-                return filepath.SkipDir
-            }
-            // Skip files whose extensions are not included
-            ext := strings.TrimSpace(filepath.Ext(path))
-            if !slices.Contains(extensions, ext) {
-                log.Default().Println("Skipping ", path)
-                return nil
-            }
-            select {
-            case pathsChan <- sourceFile{path: path, info: info}:
-                log.Default().Printf("Sent path `%s`", path)
-            case <- ctx.Done():
-                return errors.New("walk task was cancelled before finishing")
-            }
-            return nil
-        })
-        if err != nil  {
-            cancelCauseFunc(err)
-        }
-    }()
-    return pathsChan
+func walkdirGenerator(ctx context.Context, cancelCauseFunc context.CancelCauseFunc) <-chan sourceFile {
+	log.Default().Println("Walking dir")
+	pathsChan := make(chan sourceFile)
+	root := ctx.Value("root").(string)
+	extensions := ctx.Value("fileExtensions").([]string)
+	ignoredDirs := ctx.Value("ignoredDirs").([]string)
+	go func() {
+		defer close(pathsChan)
+		err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
+			log.Default().Println("Searching ", path)
+			if err != nil {
+				return err
+			}
+			if !info.Type().IsRegular() {
+				return nil
+			}
+			// Skip directories
+			if info.IsDir() && slices.Contains(ignoredDirs, info.Name()) {
+				log.Default().Println("Skipping ", path)
+				return filepath.SkipDir
+			}
+			// Skip files whose extensions are not included
+			ext := strings.TrimSpace(filepath.Ext(path))
+			if !slices.Contains(extensions, ext) {
+				log.Default().Println("Skipping ", path)
+				return nil
+			}
+			select {
+			case pathsChan <- sourceFile{path: path, info: info}:
+				log.Default().Printf("Sent path `%s`", path)
+			case <-ctx.Done():
+				return errors.New("walk task was cancelled before finishing")
+			}
+			return nil
+		})
+		if err != nil {
+			cancelCauseFunc(err)
+		}
+	}()
+	return pathsChan
 }
